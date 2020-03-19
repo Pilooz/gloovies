36217199160ef2fb0304f543507de2385a40cc48
Pierre-Gilles Levallois <pierregille.levallois@gmail.com>
2020-03-19 10:11:31 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+
+ Ressources  : 
+  - https://playground.arduino.cc/Main/MPU-6050/
+  
+*/
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
